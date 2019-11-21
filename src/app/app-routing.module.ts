9a62ce29ee8e06484c04302b6994c14f61da9b02
tbenson <tbenson@backwardgraphics.com>
2019-11-20 17:57:08 -0700

    Working on angular follow along TUT

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path: '/first/', component: FirstComponent},
+  // {path: '/second/', component: SecondComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'upd';
+  // title = 'uPawnDirect';
+  title = 'Tour of Heroes';
 }
